ac5fd6197232c43c0a83872e70c2ea9ed474193a
ajamol2197 <ajamol2197@gmail.com>
2025-11-17 15:23:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+# 1-m
+a = "abcdefghij"
+print(a[2:-5])
+
+# 2-m
+a = "HelloWorld"
+print(a[:5])
+
+# 3-m
+a = "pythonpragramming"
+print(a)
+
+t = a[7:]
+print(t)
+
+# 4-m
+a = "abcdefghijkl"
+print(a)
+
+t = a[::-1]
+print(t)
+
+# 5-m
+a  = "0123456789"
+print(a)
+
+t = a[0:10:2]
+print(t)
+
+# 6-m
+a = "welcometopython"
+print(a[3:-5])
+
+# 7-m
+a = "pythonlangunge"
+print(a[3:-4])
+
+# 8-m
+a = "ABDCEFGHIJKLM"
+print(a)
+
+t = a[5:-3]
+print(t)
+
+# 9-m
+a = "superprogramming"
+print(a)
+
+t = a[::-4]
+print(t)
+
+# 10-m
+a  = "0123456789"
+print(a)
+
+t = a[0:10:2]
+print(t)
